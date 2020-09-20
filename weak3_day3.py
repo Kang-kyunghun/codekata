0e026f2e16fe23267c590c766118222ea22f713e
Kang-Kyunghun <korea271150@gmail.com>
2020-09-20 23:11:40 +0900

    solve: weak3_day3

--- weak3_day3.py
+++ weak3_day3.py
@@ -1,15 +1,46 @@
+memo = {}
 def minPathSum(grid):
-  m = len(grid) -1
-  n = len(grid[0]) -1
-  result =[]
-  start = grid[0][0]
-  while True:
-    stat
-  return
+  def loof(x,y,gird):
+    point = '{},{}'.format(x, y)
+    if point == '0,0':
+      memo[point] = grid[0][0]
+      return grid[0][0]
+    elif point in memo.keys():
+      return memo[point]
+    else:
+      if x == 0:
+        point_sum = memo['{},{}'.format(x, y-1)]+grid[x][y]
+        memo[point] = point_sum
+        return memo[point]
+      elif y == 0:
+        point_sum = memo['{},{}'.format(x-1, y)]+grid[x][y]
+        memo[point] = point_sum
+        return memo[point]
+      else:
+        point_sum = min(memo['{},{}'.format(x, y-1)]+grid[x][y], memo['{},{}'.format(x-1, y)]+grid[x][y])
+        memo[point] = point_sum
+        return memo[point]
+  
+  for x in range(len(grid)):
+    for y in range(len(grid[0])):
+      result = loof(x,y,grid)
+  return result
 
-minPathSum([
-  [1,3,1],
-  [1,5,1],
-  [4,2,1]
-]
-) 
+'''
+모범 답안
+def minPathSum(grid):
+    m = len(grid)
+    n = len(grid[0])
+    
+    for i in range(1, n):
+        grid[0][i] += grid[0][i-1]
+        
+    for i in range(1, m):
+        grid[i][0] += grid[i-1][0]
+        
+    for i in range(1, m):
+        for j in range(1, n):
+            grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+            
+    return grid[-1][-1]
+    '''
