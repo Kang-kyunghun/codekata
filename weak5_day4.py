d8418b9508b297a79ab730cd737f6c1d018dcee6
Jack-earlysloth <kgh239@earlysloth.com>
2020-12-04 10:47:21 +0900

    weak5_day4

---
+++ weak5_day4.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, value):
+        self.val = value
+        self.next = self.pre = None
+  
+
+class MyLinkedList(Node):
+  
+  def __init__(self):
+      #
+      
+      
+  def get(self, index):
+      """
+      :type index: int
+      :rtype: int
+      """
+      
+  
+  def addAtHead(self, val):
+      """
+      :type val: int
+      """
+      
+  
+  def addAtTail(self, val):
+      """
+      :type val: int
+      """
+      
+  
+  def addAtIndex(self, index, val):
+      """
+      :type index: int
+      :type val: int
+      """
+      
+  
+  def deleteAtIndex(self, index):
+      """
+      :type index: int
+      """
+      
