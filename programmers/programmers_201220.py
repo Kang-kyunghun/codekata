09fd7fb5b4c1e4cfbb9305184c7b7c36a10bba24
Jack-earlysloth <kgh239@earlysloth.com>
2020-12-20 19:23:22 +0900

    programmers_201220

---
+++ programmers/programmers_201220.py
@@ -0,0 +1,11 @@
+#2016년
+
+def solution(a, b):
+    day = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED" ]
+    num = (a//2)*31 + ((a-1)//2)*30 + b
+    if a > 2:
+        num = num -1
+    if a in [9,11]:
+        num += 1
+    answer = day[num%7]
+    return answer
