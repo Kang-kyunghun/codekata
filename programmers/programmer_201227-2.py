6a893a78500bc15a9336275a259862fb97459eb3
Kang-kyunghun <korea271150@gmail.com>
2020-12-27 16:06:17 +0900

    while -> for

--- programmers/programmer_201227-2.py
+++ programmers/programmer_201227-2.py
@@ -1,14 +1,13 @@
 def solution(plain):
     index = 0
     add = ''
-    while True:
-        l = len(plain) // 2
+    for index in range(len(plain)):
         re_plain = plain+add[::-1]
         print( re_plain, re_plain[::-1])
         if re_plain == re_plain[::-1]:
             return re_plain
         add += plain[index]
-        index += 1
+       
 
 
 
